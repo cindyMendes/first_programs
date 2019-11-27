48480de15c83a9e47a63a3edfef28261ce2b4f9e
Ubuntu <ubuntu@ip-172-31-93-199.ec2.internal>
2019-11-27 21:44:25 +0000

    Creating you own classes

---
+++ sums.rb
@@ -0,0 +1,42 @@
+# Create a program sums.rb with two classes.  
+# Create this in the first_programs directory so that you can upload it to github as usual.
+# - a class called Sum1 with an initialize method that takes two parameters and 
+# sets the instance variable total to the sum of the two.  Include a line at the top of the class: 
+# attr_accessor :total
+# - a class called Sum2 with an initialize method that takes two parameters a and b and sets 
+# the instance variable a to the parameter a and the instance variable b to the parameter b.  
+# Create a method new_total inside Sum2 that returns the sum of the instance variables a and b.
+# - create instances of Sum1 and Sum2, passing 5 and 6 as parameters on the new statement. 
+# Print out the total for Sum1.  Print out the new_total for Sum2.
+
+
+class Sum1
+    attr_accessor :total
+
+    def initialize(param1, param2)
+        @total = param1 + param2
+    end
+    
+end 
+
+class Sum2 < Sum1
+    attr_accessor :a, :b
+    
+    def initialize(a, b)
+        @a = a
+        @b = b
+    end 
+    
+    def new_total
+        return @a + @b
+    end 
+    
+end 
+
+sum1 = Sum1.new(10, 12)
+sum2 = Sum2.new(5, 6)
+
+puts sum1.total
+puts sum2.new_total
+        
+    
